46f07e7cb3d0853335082ad7343a2f3d7fb1df7b
Larissa Brasil <larissabrasil009@gmail.com>
2025-10-04 18:49:18 -0300

    feat: envolve o componente SideBar com MenuProvider no Welcome

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,14 +1,14 @@
-import { SideBar, itemSelected } from "~/components/side-bar";
-import logoDark from "./logo-dark.svg";
-import logoLight from "./logo-light.svg";
+import { MenuProvider, SideBar } from "~/components/side-bar";
 import { Dashboard } from "~/components/dashboard";
 
 
 export function Welcome() {
   return (
     <div className="flex flex-row">
-      <SideBar />
-      <Dashboard />
+      <MenuProvider>
+        <SideBar />
+        <Dashboard />
+      </MenuProvider>
     </div>
   );
 }
