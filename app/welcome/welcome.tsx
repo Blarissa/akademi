cf17adda29b2464b813d48f2a79e9b5132bef117
Larissa Brasil <larissabrasil009@gmail.com>
2025-10-05 20:39:48 -0300

    feat: ajusta layout da página de boas-vindas para responsividade e melhora estilo

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -3,10 +3,10 @@ import { Outlet } from "react-router";
 
 export default function Welcome() {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-col lg:flex-row min-h-screen">
       <MenuProvider>
         <SideBar />
-        <main className="flex-1">
+        <main className="flex-1 w-full overflow-x-hidden bg-[#f3f4ff]">
           <Outlet />
         </main>
       </MenuProvider>
